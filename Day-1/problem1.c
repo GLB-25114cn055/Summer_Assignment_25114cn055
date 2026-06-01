4b3db1612e06e441426c3c6b3ed2b5dd1e0da3c9
GLB-25114cn055 <abhijitbarick27@gmail.com>
2026-06-01 13:41:15 +0530

    Day-1 assignment is uploaded

---
+++ Day-1/problem1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n, i, sum = 0;
+    printf("enter a natural number N: ");
+    scanf("%d",&n);
+
+    for(i=1;i<=n;i++)
+    {
+        sum = sum + i;
+    
+    }
+    printf("sum of %d natural number = %d",n,sum);
+    return 0;
+}
